3231ebd884734e96166989a0162643269f01a4c3
Yuto Tachibana <tachibanayu24@gmail.com>
2022-01-15 16:58:34 +0900

    Install ReactFlow

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,14 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 
+import { Flow } from "@/components/Flow";
+
 const Home: NextPage = () => {
   return (
     <div className="dark">
-      <div className="flex flex-col min-h-screen dark:text-gray-50 bg-gray-100 dark:bg-gray-800">
+      <div className="flex flex-col min-h-screen antialiased hover:subpixel-antialiased dark:text-gray-50 bg-gray-100 dark:bg-gray-800">
         <Head>
-          <title>Share Flow Chart</title>
+          <title>📈 Share Flow Chart</title>
           <link
             href="http://fonts.googleapis.com/css?family=Ubuntu&subset=cyrillic,latin"
             rel="stylesheet"
@@ -20,8 +22,11 @@ const Home: NextPage = () => {
         <main className="grow">
           <body>
             <div className=" p-4">
-              <h1 className="text-4xl font-bold text-center">Share Flow Chart</h1>
+              <h1 className="text-4xl font-bold text-center">📈 Share Flow Chart</h1>
             </div>
+            {/* <div className="flex justify-center pt-8"> */}
+            <Flow isDarkMode />
+            {/* </div> */}
           </body>
         </main>
 
